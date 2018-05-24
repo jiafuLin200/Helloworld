9d92288b8debbde5f04a624156b1e3851634f44e
jiafuLin200 <linjf200@126.com>
2018-05-24 11:13:25 +0800

    测试ea

---
+++ src/main/java/com/jiafu/test/TestGit.java
@@ -0,0 +1,14 @@
+package com.jiafu.test;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("this project is for testing git");
+    }
+}
+
+
+
+
+
+
